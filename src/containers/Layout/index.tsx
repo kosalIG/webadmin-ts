768d0fa6bad700f1ff5294be58df6ea66ca71270
kosal73 <kosalarrupe@gmail.com>
2021-06-04 18:02:53 +0700

    done fix navbar

--- src/containers/Layout/index.tsx
+++ src/containers/Layout/index.tsx
@@ -2,11 +2,14 @@ import React, { Suspense } from 'react';
 import { useState } from 'react';
 import { Switch, Route, useLocation } from 'react-router-dom';
 import Loading from 'components/Loading';
+import { Avatar } from 'antd';
 
 import route from '_route';
 import nav from '_nav';
 import { Header, SideBar } from './components';
 import { WrapSider, WrapContent, CustomLoading, Footer, Layout, Logo, SubLogo } from './style';
+import logoImage from 'images/admin-logo/logo.jpg';
+
 const Index: React.FC = () => {
     const [collapsed, setCollapsed] = useState<boolean>(false);
     const { pathname } = useLocation();
@@ -15,6 +18,7 @@ const Index: React.FC = () => {
         <Layout style={{ overflow: 'hidden' }}>
             <WrapSider collapsed={collapsed} onCollapse={() => setCollapsed(!collapsed)} breakpoint="sm" collapsible>
                 <Logo>
+                    <Avatar src={logoImage} />
                     <SubLogo>QWIQ ADMIN</SubLogo>
                 </Logo>
                 <SideBar pathname={pathname} nav={nav} />
@@ -36,11 +40,12 @@ const Index: React.FC = () => {
                                         <Route key={r.id} exact={r.exact} path={r.path} component={r.component} />
                                     ))}
                                     {/* <Redirect from="/" to="/dashboard" /> */}
+                                    {/* <Route render={() => <Test />} /> */}
                                 </div>
                             </React.Fragment>
                         </Switch>
                     </Suspense>
-                    <Footer style={{ textAlign: 'center' }}>PAGE ADMIN CORE ©2020 Created by KOSAL</Footer>
+                    <Footer style={{ textAlign: 'center' }}>PAGE ADMIN ©2021 Created by QWIQ Team</Footer>
                 </WrapContent>
             </Layout>
         </Layout>
